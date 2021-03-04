df8785b45641290c95a600b3e2e53aa3990f5002
Laurens <laurens.vijnck@selligent.com>
2021-03-04 17:31:41 +0100

    add dummy api

---
+++ services/api/scripts/002_run_local.sh
@@ -0,0 +1,8 @@
+# Run the API and mount credentials into the docker container
+docker run \
+    -e PORT=8080 \
+    -v $GOOGLE_APPLICATION_CREDENTIALS:/tmp/keys/sa.json:ro \
+    -e PYTHONUNBUFFERED=TRUE \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/tmp/keys/sa.json \
+    -p 8080:8080 \
+    speech-api
